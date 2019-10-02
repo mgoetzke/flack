4c8c96343bd4615914d92dd58e7962666661feeb
mgoetzke <mgoetzke@gmail.com>
2019-10-01 23:30:28 -0400

    Add error for previous sign-up with username

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::UsersController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotUnique, with: :render_duplicate_email_response
   def create
     @user = User.new(user_params)
     if @user.save
@@ -22,5 +24,9 @@ class Api::UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :display_name, :password)
+  end
+
+  def render_duplicate_email_response
+    render json: ["Account already exists for this email"], status: :not_found
   end
 end
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -7,7 +7,7 @@ export default (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
-      return action.errors;
+      return state.concat(action.errors);
     case RECEIVE_CURRENT_USER:
       return [];
     default:
