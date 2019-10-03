0ae30c90e57f40c98b2e646d091f9d652c58d16b
mgoetzke <mgoetzke@gmail.com>
2019-10-02 23:49:29 -0400

    Add splash animation and navigation popup

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,23 +5,34 @@ class Greeting extends React.Component {
   constructor(props){
     super(props);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleMenu = this.handleMenu.bind(this);
   }
 
   handleLogout() {
     this.props.logout(this.props.currentUser);
   }
+
+  handleMenu(){
+    const popup = document.getElementById("menuPopup");
+    popup.classList.toggle("show");
+  }
   render() {
     const buttonPath = (this.props.location.pathname === "/signup") ? "/login" : "/signup"
-    const buttonText = (this.props.location.pathname === "/signup") ? "Sign In" : "Sign Up"
+    const buttonText = (this.props.location.pathname === "/signup") ? "Sign in" : "Sign up"
     const buttonStyle = (this.props.location.pathname === "/") ? "greeting-button" : "greeting-nav-button"
 
 
     if (this.props.currentUser) {
       let name = this.props.currentUser.display_name;
       return (
-        <div>
-          <h1>{name}</h1>
-          <button onClick={this.handleLogout}> Log Out </button>
+        <div className="greeting-loggedIn" onClick={this.handleMenu}>
+
+          <h2>Flack<span> v </span></h2>
+          <span>{name}</span>
+          <div className="greeting-nav-popup" id="menuPopup">
+            <p>{name}</p>
+            <button onClick={this.handleLogout}> Log Out </button>
+          </div>
         </div>
       );
     } else {
