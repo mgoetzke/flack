f1f92e42c73312009cf52c5cb351e683a76577ca
mgoetzke <mgoetzke@gmail.com>
2019-10-03 21:06:24 -0400

    Create message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_001935) do
+ActiveRecord::Schema.define(version: 2019_10_04_010249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2019_10_04_001935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "memberable_id"], name: "index_memberships_on_user_id_and_memberable_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "author_id", null: false
+    t.integer "messageable_id", null: false
+    t.string "messageable_type", null: false
+    t.index ["author_id"], name: "index_messages_on_author_id"
+    t.index ["messageable_id"], name: "index_messages_on_messageable_id"
   end
 
   create_table "users", force: :cascade do |t|
