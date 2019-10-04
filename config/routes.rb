39e464357ef368790e9f7a94980640f65aac9f1e
mgoetzke <mgoetzke@gmail.com>
2019-10-04 09:02:39 -0400

    Create chat channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: "static_pages#root"
+  # root to: "static_pages#root"
+  root to: "api/messages#root"
+  mount ActionCable.server, at: '/cable'
   namespace :api, defaults: {format: "json"} do
     resources :users, only: [:create, :update, :destroy, :show]
     resource :session, only: [:create, :destroy, :update, :show]
--- frontend/components/channel/channel_container.jsx
+++ frontend/components/channel/channel_container.jsx
@@ -1,22 +1,33 @@
 import { connect } from "react-redux";
-import Channel from './channel';
-import {fetchChannel} from '../../actions/channel_actions';
-import { withRouter } from 'react-router-dom';
-import {fetchAllMessages} from '../../actions/message_actions';
+import Channel from "./channel";
+import { fetchChannel } from "../../actions/channel_actions";
+import { withRouter } from "react-router-dom";
+import { fetchAllMessages } from "../../actions/message_actions";
 
 const mapState = (state, ownProps) => {
-  let default_channel = {name: "Default channel", topic: "defaulty", private: false, admin_id: 1};
-  let channel = state.entities.channels[ownProps.match.params.channelId] || default_channel;
-  return ({
+  let default_channel = {
+    name: "Default channel",
+    topic: "defaulty",
+    private: false,
+    admin_id: 1
+  };
+  let channel =
+    state.entities.channels[ownProps.match.params.channelId] || default_channel;
+  return {
     channel: channel,
-  });
-}
+    messages: state.entities.messages
+  };
+};
 
 const mapDispatch = dispatch => {
-  return ({
-    fetchChannel: (id) => dispatch(fetchChannel(id)),
+  return {
+    fetchChannel: id => dispatch(fetchChannel(id)),
     fetchAllMessages: () => dispatch(fetchAllMessages())
-
-  });
-}
-export default withRouter(connect(mapState, mapDispatch)(Channel));
+  };
+};
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(Channel)
+);
