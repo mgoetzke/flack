446e03c9bb5455b85674b6bf6eeae0d8d05ad0db
mgoetzke <mgoetzke@gmail.com>
2019-10-05 10:05:45 -0400

    Implement logic for edit form

--- frontend/components/message/message.jsx
+++ frontend/components/message/message.jsx
@@ -3,29 +3,35 @@ import React from "react";
 class Message extends React.Component {
   constructor(props) {
     super(props);
-    this.state = props.message;
-    this.handleEdit = this.handleEdit.bind(this);
+    this.state = { message: props.message, editing: false };
+    this.handleEditButton = this.handleEditButton.bind(this);
   }
-  handleEdit() {
-    this.props.updateMessage(this.state);
+  handleEditButton() {
+    let newEditState = this.state.editing === false ? true : false;
+    this.setState({ editing: newEditState });
   }
   update(field) {
     return e => this.setState({ [field]: e.currentTarget.value });
   }
 
   render() {
-    let image_location = this.state.image_url.split(".")[0];
+    let image_location = this.state.message.image_url.split(".")[0];
+    let testbg = this.state.editing ? "test-blue" : "";
     return (
-      <div>
-        {this.state.display_name}
+      <>
         <img className="message-avatar" src={window[image_location]} />
-        <div>{this.state.body}</div>
-        {this.state.created_at}
-        {this.state.updated_at}
-        <button onClick={this.handleEdit} className="message-edit">
-          Edit
-        </button>
-      </div>
+        <div>
+          <div className={testbg}>
+            {this.state.message.display_name}
+            <div>{this.state.message.body}</div>
+            {this.state.message.created_at}
+            {this.state.message.updated_at}
+          </div>
+          <button onClick={this.handleEditButton} className="message-edit">
+            Edit
+          </button>
+        </div>
+      </>
     );
   }
 }
