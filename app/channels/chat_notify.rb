0da23e9fb07f272ce50e5f88271bae5d67c5565c
mgoetzke <mgoetzke@gmail.com>
2019-10-11 07:09:20 -0400

    Fix channel broadcast

--- app/channels/chat_notify.rb
+++ app/channels/chat_notify.rb
@@ -3,8 +3,8 @@ class NotificationsChannel < ApplicationCable::Channel
     stream_from "notifications_#{current_user.id}"
   end
 
-  def self.sendMembership(membership)
-      socket = {message: membership, type: 'membership'}
-      ChatDirect.broadcast_to(direct.id, socket)
+  def self.sendChannel(channel)
+      socket = {message: channel, type: 'channelAdd'}
+      NotificationChannel.broadcast_to(current_user.id, socket)
   end
 end
